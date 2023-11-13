5ace4566fe09f2eadfd92e453d8e2045ebcbc58f
Dustin Campbell <dustin@teamcampbell.org>
2023-11-13 13:04:52 -0800

    Initial code for .NET Conf 2023 demo

---
+++ dotnetconf-2023/DotNetConf-2023/Fluff.cs
@@ -0,0 +1,13 @@
+﻿global using System.Collections.Immutable;
+global using static System.Console;
+
+public class Person(string name)
+{
+}
+
+public static class Grades
+{
+    private static readonly decimal[] s_dustin = { 3.9m, 3.75m, 4.0m };
+
+    public static ReadOnlySpan<decimal> Dustin => s_dustin;
+}
---
+++ dotnetconf-2023/DotNetConf-2023/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grade = System.Decimal;
+
+var mads = new Student("Mads Torgersen", 900751, new[] { 3.5m, 2.9m, 1.8m });
+
+WriteLine(mads);
+
+public record class Student(string Name, int Id, Grade[] Grades)
+{
+    public Student(string name, int id) : this(name, id, Array.Empty<Grade>()) { }
+
+    public Grade GPA => Grades switch
+    {
+        [] => 4.0m,
+        [var grade] => grade,
+        [.. var all] => all.Average()
+    };
+}
